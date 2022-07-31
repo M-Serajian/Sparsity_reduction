92c77421b29586cb0e951058bdc0ea155319fd8f
MSEali <m.serajian@ufl.edu>
2022-07-31 13:31:47 -0400

    input fasta file, function not wrapped up on class

--- src/Sparsity_reduction_header.h
+++ src/Sparsity_reduction_header.h
@@ -14,24 +14,149 @@ using namespace std;
 using namespace sdsl;
 
 
-template <class cst_t, class node_t=typename cst_t::node_type, class char_t=typename cst_t::char_type>
-struct To_be_reported {
-    public:
-        const int output_size;
-        cst_t Compressed_Suffix_tree;
-        node_t selected_nodes_array[output_size];
-        string substrings[output_size];
-};
+// template <class cst_t, class node_t=typename cst_t::node_type, class char_t=typename cst_t::char_type>
+// struct To_be_reported {
+//     public:
+//         const int output_size;
+//         cst_t Compressed_Suffix_tree;
+//         node_t selected_nodes_array[output_size];
+//         string substrings[output_size];
+// };
 
-template <class cst_t, class node_t=typename cst_t::node_type, class char_t=typename cst_t::char_type>
-void find_repetitive_sequences(const char *X, uint8_t num_bytes_X,int Threshold, int L,To_be_reported output){    
+// template <class cst_t, class node_t=typename cst_t::node_type, class char_t=typename cst_t::char_type>
+// void find_repetitive_sequences(const char *X, uint8_t num_bytes_X,int Threshold, int L,To_be_reported output){    
+//     cst_t cst;
+//     construct_im(cst, X, 1);
+
+//     node_t root= cst.root();
+//     int nodes= cst.nodes();
+//     int Node_children_based_on_id_DFS[nodes]= {0}; // Node_children_based_on_id_DFS
+//     int flag[nodes]= {0}; // a flag array for showing the number of leaves reaching to a node are higher than threshold
+//     node_t NodeCST[nodes]={}; // Nodes of CST based on DFS ID
+//     uint64_t max_depth = nodes+1;
+
+//     // use the DFS iterator to traverse `cst`
+//     for (auto it=cst.begin(); it!=cst.end(); ++it) {
+//         if (it.visit() == 1) {  // node visited the first time
+//             auto v = *it;       // get the node by dereferencing the iterator
+//             if (cst.depth(v) <= max_depth){// if depth node is <= max_depth
+//                 // process node, e.g. output it in format d-[lb, rb]
+                
+//                 Node_children_based_on_id_DFS[cst.id(v)]=1;
+//                 NodeCST[cst.id(v)]=v; 
+//             } else { // skip the subtree otherwise
+//                 it.skip_subtree();
+//             }
+//         }
+
+//         if (it.visit() == 2){
+//             auto v = *it;
+//             Node_children_based_on_id_DFS[cst.id(v)]=0; // it means the node is internal so there is no point in adding them (our mistake)
+//             // only the number of leaves that are rooted to a specific internal node are important for 
+            
+//                 for (auto& child: cst.children(v)){
+                    
+//                 Node_children_based_on_id_DFS[cst.id(v)]=Node_children_based_on_id_DFS[cst.id(v)]+ Node_children_based_on_id_DFS[cst.id(child)];
+//                 }
+//         }
+//     }
+
+
+
+//     // Flagging the number leaves rooting a node with id= i
+//     for (int i=0;i<nodes-1;i++){ //-1 is because the root of the CST is not important for us. infact its associate with an empty string (usually shown as epsilon in litreture)
+
+//     if (Threshold <= Node_children_based_on_id_DFS[i]  ){
+//         flag[i]=1;
+//     }
+//     }
+
+
+//     // 15: Let C be the set of all suffix chains for all the nodes in A.
+//     // 16: for each chain c in C do ▷ Finds the subsequences that can not be
+//     // extended from left
+//     // here we did not calculate the C, since it was not needed, just for each flaged node, we check the slink until we reach the top of the tree(root)
+//     // 17: Find the node with lowest height in the chain
+//     // 18: Ignore all other nodes in that chain
+//     // Store that node into B.
+//     // 20: end for
+//     // 21: Return T (here T is equivalent to the array __ flag __ )
+
+
+//     node_t slink_p, node; // slink_p = suffix like of the node, node= node that is recalled based on its id 
+//     for (int i=0;i<nodes-1;i++){// -1 since root is not important and its id is always = nodes (total number of nodes)
+
+//         if(flag[i]==1){
+//             node= NodeCST[i];
+//             slink_p= cst.sl(node);
+//             while(slink_p!=root){
+//                 if (flag[cst.id(slink_p)]==1){
+//                     if (cst.depth(node)<cst.depth(slink_p)){
+//                         flag[cst.id(node)]=0;
+//                     }
+//                     else{
+//                         flag[cst.id(slink_p)]=0;
+//                     }
+//                 }
+//                 node=slink_p;
+//                 slink_p=cst.sl(node);
+
+//             }
+
+
+
+//         }
+//     }
+// int counter=0;
+// for (int i=0;i<nodes;i++){
+//     if(flag[i]==1){
+//         counter++;
+//     }  
+// }
+// node_t output_array_nodes[counter]={};
+// counter=0;
+// for (int i=0;i<nodes;i++){
+//     if(flag[i]==1){
+//         output_array_nodes[counter]=NodeCST[i];
+//         counter++;
+//     }  
+// }
+
+    
+// for (int i=0;i<nodes;i++){
+    
+//         if(flag[i]==1){
+//         cout<< "For the node with id=  "<< i <<", the substring which is repeated at least " << Threshold <<" times is : " << extract(cst, NodeCST[i]) <<endl;
+//         }
+    
+//     }
+
+// output.output_size=counter;
+// output.Compressed_Suffix_tree=cst;
+// output.selected_nodes_array=output_array_nodes;
+// for (int i=0;i<counter;i++){
+//     output.substrings[i]=extract(cst,output.selected_nodes_array[i]);
+// }
+// return;
+// }   
+// // void repeated_substring_printer(To_be_reported output){
+
+// //     for (int i=0;i<output.output_size;i++){
+// //         cout<<output.substrings[i]<<endl;  
+// //     }
+// //     return;
+// // }
+
+typedef cst_sct3<csa_bitcompressed<int_alphabet<> > >::node_type node_t;
+//template <class cst_t, class node_t=typename cst_t::node_type, class char_t=typename cst_t::char_type>
+
+void find_repetitive_sequences (const char *X, uint8_t num_bytes_X,int k, int l){    
     cst_t cst;
     construct_im(cst, X, 1);
-
     node_t root= cst.root();
     int nodes= cst.nodes();
     int Node_children_based_on_id_DFS[nodes]= {0}; // Node_children_based_on_id_DFS
-    int flag[nodes]= {0}; // a flag array for showing the number of leaves reaching to a node are higher than threshold
+    int flag[nodes]= {0}; // a flag array for showing the number of leaves reaching to a node are higher than threshold=k
     node_t NodeCST[nodes]={}; // Nodes of CST based on DFS ID
     uint64_t max_depth = nodes+1;
 
@@ -66,7 +191,7 @@ void find_repetitive_sequences(const char *X, uint8_t num_bytes_X,int Threshold,
     // Flagging the number leaves rooting a node with id= i
     for (int i=0;i<nodes-1;i++){ //-1 is because the root of the CST is not important for us. infact its associate with an empty string (usually shown as epsilon in litreture)
 
-    if (Threshold <= Node_children_based_on_id_DFS[i]  ){
+    if (k <= Node_children_based_on_id_DFS[i]  ){
         flag[i]=1;
     }
     }
@@ -108,10 +233,16 @@ void find_repetitive_sequences(const char *X, uint8_t num_bytes_X,int Threshold,
         }
     }
 int counter=0;
+//checking the length of the substrings
 for (int i=0;i<nodes;i++){
     if(flag[i]==1){
-        counter++;
-    }  
+        int d=cst.depth(NodeCST[i]);
+        if (d >= l){
+            counter++;
+        }else {
+            flag[i]=0;
+        }
+        }  
 }
 node_t output_array_nodes[counter]={};
 counter=0;
@@ -122,27 +253,49 @@ for (int i=0;i<nodes;i++){
     }  
 }
 
-    
 for (int i=0;i<nodes;i++){
-    
-        if(flag[i]==1){
-        cout<< "For the node with id=  "<< i <<", the substring which is repeated at least " << Threshold <<" times is : " << extract(cst, NodeCST[i]) <<endl;
-        }
-    
+    if(flag[i]==1){
+    cout<< "For the node with id=  "<< i <<", the substring which is repeated at least " << k <<" times is : " << extract(cst, NodeCST[i]) <<endl;
     }
-
-output.output_size=counter;
-output.Compressed_Suffix_tree=cst;
-output.selected_nodes_array=output_array_nodes;
-for (int i=0;i<counter;i++){
-    output.substrings[i]=extract(cst,output.selected_nodes_array[i]);
 }
 return;
-}   
-// void repeated_substring_printer(To_be_reported output){
+}
 
-//     for (int i=0;i<output.output_size;i++){
-//         cout<<output.substrings[i]<<endl;  
-//     }
-//     return;
-// }
+
+
+string input_file_reader (string address)
+{
+    ifstream Input_file;
+    Input_file.open(address);
+
+    string line, name /*,content*/;
+    string out="";
+    
+    while( getline( Input_file, line ).good() ){
+        if( line.empty() || line[0] == '>' ){ // Identifier marker- For new Lines
+            if( !name.empty() ){ // Print out what we read from the last entry
+                name.clear();
+            }
+            if( !line.empty() ){
+                name = line.substr(1);
+                //cout<<name <<endl;
+                out=out+name +"$";           
+            }
+            //content.clear();
+        }
+        else if( !name.empty() ){
+            if( line.find(' ') != string::npos ){ // Invalid sequence--no spaces allowed
+                name.clear();
+                //content.clear();
+            } else {
+                //content += '$';              
+            }
+        }
+    }  
+    
+    out.erase(out.size()-1,1);// removing the last charater of the string ($) since 
+    // CST ads one to the end of string on its own. we do not want to have $$ at the of sequence
+    // end!
+    return (out);
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -22,19 +22,103 @@ typedef cst_sct3<csa_bitcompressed<int_alphabet<> > >::node_type node_t;
 // recalling the classes defined in this project
 # include "Sparsity_reduction_header.h"
 
+//*********************** Argument options ***************************************
+// struct containing command line parameters and other globals
+// struct Args
+// {
+//   std::string input = "in.fa";
+//   std::string output   = "out.fa"; // output file prefix
+//   size_t l = 100;             // minumum MEM length
+//   size_t k = 1;              // number of repeatitions
+//   size_t th = 1;             // number of threads
+// };
+
+// void parseArgs(int argc, char *const argv[], Args &arg)
+// {
+//   int c;
+//   extern char *optarg;
+//   extern int optind;
+
+//   std::string usage("usage: " + std::string(argv[0]) + " infile [-i input] [-o output] [-t threads] [-l len] [-k repetitions] \n\n" +
+//                     "Copmputes unique substrings occuring at least k times with min lenght of l\n" +
+//                     "    k: [integer]  - min occurances of the substring.\n" +
+//                     "    len: [integer]  - min length of repeated substring.\n" +
+//                     "    input: [string]  - input file prefix.\n" +
+//                     "    output: [string]  - output file prefix.\n" +
+//                     "    thread: [integer] - number of threads (def. 1)\n");
+
+//   std::string sarg;
+//   while ((c = getopt(argc, argv, "l:hp:o:t:")) != -1)
+//   {
+//     switch (c)
+//     {
+//     case 'i':
+//       arg.input.assign(optarg);
+//       break;
+//     case 'o':
+//       arg.output.assign(optarg);
+//       break;
+//     case 'l':
+//       sarg.assign(optarg);
+//       arg.l = stoi(sarg);
+//       break;
+//     case 't':
+//       sarg.assign(optarg);
+//       arg.th = stoi(sarg);
+//       break;
+//     case 'k':
+//       sarg.assign(optarg);
+//       arg.k = stoi(sarg);
+//     case '?':
+//       error("Unknown option.\n", usage);
+//       exit(1);
+//     }
+//   }
+//   // the only input parameter is the file name
+//   if (argc == optind + 1)
+//   {
+//     arg.input.assign(argv[optind]);
+//   }
+//   else
+//   {
+//     error("Invalid number of arguments\n", usage);
+//   }
+// }
 
 #define DEBUG 
 
 
 int main(int argc, char *const argv[])
 {
+string in_address= "/s/nut/a/homes/m.serajian/projects/Sparsity_reduction/TestData/data.fa";
+string out_address= "/s/nut/a/homes/m.serajian/projects/Sparsity_reduction/TestData/output.fa";
+string A=  input_file_reader(in_address);
+const char* out;
+out= A.c_str();
+cout << out <<endl;
+// add a function for readin
 
+find_repetitive_sequences(out,1,10,2);
 
 #ifdef DEBUG // Check to see if flag is defined
 
-const char * text= "ACGGCACGAATACG";
+//Args args;
+//parseArgs(argc, argv, args);
 
-int k;
+// step2
+// read input ::istream, 
+// Do it in a function in main.cpp, call that function
+// kseq header file for this function-Max's code
+
+// Step 3
+// instantiate the class and building the cst
+// inside the class
+// inside the header file (SR), output, fasta file
+
+//find_repetitive_sequences(out, 1,5);
+
+ofstream output;
+
 
 #endif // DEB
 
